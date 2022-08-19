fb53eb722305e26ffe88e0cedb0f2969b6472e8e
sutanto7475 <76191538+sutanto7475@users.noreply.github.com>
2022-08-19 08:18:58 -0500

    8/19/22
    findSoln

--- app/src/main/java/com/example/actualcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/actualcalculatorapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.actualcalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void findSoln()
+    public void findSoln(View view){
+        Intent intent = getIntent();
+        String nameToShow = intent.getStringExtra("NAME");
+
+        TextView textView = findViewById(R.id.userName);
+        textView.setText(nameToShow);
+    }
 }
